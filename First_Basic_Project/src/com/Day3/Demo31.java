a7421b317e4e435dae056279250e55be8f789afe
md uddin <md.uddin@revature.net>
2021-07-28 17:34:16 -0400

    day 3 added

--- First_Basic_Project/src/com/Day3/Demo31.java
+++ First_Basic_Project/src/com/Day3/Demo31.java
@@ -1,7 +1,25 @@
 package com.Day3;
 
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+
 public class Demo31 {
     public static void main(String[] args) {
+        // creating an object of fileOutputStream
+        try(FileOutputStream stream=new FileOutputStream("D:\\JavaFullStack\\First_Basic_Project\\src\\com\\Day3\\data.txt"))
+        {
+            // custom string input
+            String data="Hi, I am writing from here";
+            byte[] arr=data.getBytes();
+            // convert string to bytes
 
+            stream.write(arr);
+
+            // catch block to handle exception
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("stream closed");
     }
 }
